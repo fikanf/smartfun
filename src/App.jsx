28ab42777e1564b96f804a996278fbbb2f36b559
AdamAbd <adamabd14113@gmail.com>
2023-08-22 20:52:27 +0700

    add video

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
+import awal from "./assets/video/awal.mp4";
+
 function App() {
   return (
-    <>
-      <h1 className="text-3xl text-red-50 font-bold underline">Hello world!</h1>
-    </>
+    <div className="static">
+      <video autoPlay muted loop className="w-screen h-screen object-cover">
+        <source src={awal} type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+      <div className="absolute top-0">
+        <h1 className="text-3xl text-red-50 font-bold underline">
+          Hello world!
+        </h1>
+      </div>
+    </div>
   );
 }
 

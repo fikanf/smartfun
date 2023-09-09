82d6042cf7c3372dfce323604328288bcdf392e8
AdamAbd <adamabd14113@gmail.com>
2023-09-09 17:00:45 +0700

    finish setting electron

---
+++ electron/main.cjs
@@ -0,0 +1,39 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path')
+
+const createWindow = () => {
+    // Create the browser window.
+    const mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+    });
+
+    // and load the index.html of the app.
+    // mainWindow.loadURL('http://localhost:6969');
+    mainWindow.loadFile(path.join(app.getAppPath(), 'dist/index.html'));
+
+    // Open the DevTools.
+    mainWindow.webContents.openDevTools();
+};
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', createWindow);
+
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('activate', () => {
+    // On OS X it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow();
+    }
+});
